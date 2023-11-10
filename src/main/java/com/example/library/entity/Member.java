a443c734c443fa3cd3d28147beada318cee9ad8d
jungmin.park <pjm9673@gmail.com>
2023-11-10 15:26:46 +0900

    Member entity 생성

--- src/main/java/com/example/library/entity/Member.java
+++ src/main/java/com/example/library/entity/Member.java
@@ -1,4 +1,25 @@
 package com.example.library.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "member")
 public class Member {
+    @Id
+    private Long id;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "gender", nullable = false)
+    private String gender;
+
+    @Column(name = "socialId", nullable = false)
+    private String socialId;
+
+    @Column(name = "phoneNumber", nullable = false)
+    private String phoneNumber;
+
+    @Column(name = "address", nullable = false)
+    private String address;
 }
